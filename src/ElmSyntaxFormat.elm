f88d62dc1b94bfbe0bd4b1289cd54562e3ea8a85
lue-bird <lue.the.bird@gmail.com>
2024-09-12 15:37:51 +0200

    fully implement imports

--- src/ElmSyntaxFormat.elm
+++ src/ElmSyntaxFormat.elm
@@ -12,7 +12,7 @@ import Elm.Syntax.Import
 import Elm.Syntax.ModuleName
 import Elm.Syntax.Node
 import Elm.Syntax.Range
-import Print exposing (Print)
+import Print exposing (LineOffset, Print)
 
 
 module_ : Elm.Syntax.File.File -> Print
@@ -22,7 +22,13 @@ module_ syntaxModule =
     -- TODO comments
     Print.inSequence
         (syntaxModule.imports
-            |> List.map (\(Elm.Syntax.Node.Node _ syntaxImport) -> import_ syntaxImport)
+            |> List.map (\(Elm.Syntax.Node.Node _ syntaxImport) -> syntaxImport)
+            |> List.sortWith
+                (\a b ->
+                    compare (a.moduleName |> Elm.Syntax.Node.value) (b.moduleName |> Elm.Syntax.Node.value)
+                )
+            |> importsCombine
+            |> List.map (\syntaxImport -> import_ syntaxImport)
             |> List.intersperse Print.linebreak
         )
 
@@ -75,19 +81,128 @@ import_ syntaxImport =
                         |> Print.followedBy
                             (Print.bumpIndent
                                 (Print.layoutPositiveIndent lineOffset
-                                    |> Print.followedBy (exposing_ syntaxExposing)
+                                    |> Print.followedBy (exposing_ lineOffset syntaxExposing)
                                 )
                             )
             )
 
 
+importsCombine :
+    List Elm.Syntax.Import.Import
+    -> List Elm.Syntax.Import.Import
+importsCombine imports =
+    case imports of
+        [] ->
+            []
+
+        [ onlyImport ] ->
+            [ onlyImport |> importToNormal ]
+
+        import0 :: import1 :: import2Up ->
+            if (import0.moduleName |> Elm.Syntax.Node.value) == (import1.moduleName |> Elm.Syntax.Node.value) then
+                importsCombine (importsMerge import0 import1 :: import2Up)
+
+            else
+                (import0 |> importToNormal) :: importsCombine (import1 :: import2Up)
+
+
+importToNormal : Elm.Syntax.Import.Import -> Elm.Syntax.Import.Import
+importToNormal syntaxImport =
+    { moduleName = syntaxImport.moduleName
+    , moduleAlias = syntaxImport.moduleAlias
+    , exposingList =
+        syntaxImport.exposingList |> exposingToNormal
+    }
+
+
+exposeListToNormal :
+    List (Elm.Syntax.Node.Node Elm.Syntax.Exposing.TopLevelExpose)
+    -> List (Elm.Syntax.Node.Node Elm.Syntax.Exposing.TopLevelExpose)
+exposeListToNormal syntaxExposeList =
+    syntaxExposeList
+        |> List.map Elm.Syntax.Node.value
+        |> List.sortWith exposeCompare
+        |> exposesCombine
+        |> List.map Elm.Syntax.Node.empty
+
+
+exposingToNormal :
+    Maybe (Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing)
+    -> Maybe (Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing)
+exposingToNormal syntaxExposing =
+    case syntaxExposing of
+        Just (Elm.Syntax.Node.Node exposingAllRange (Elm.Syntax.Exposing.All allRange)) ->
+            Just (Elm.Syntax.Node.Node exposingAllRange (Elm.Syntax.Exposing.All allRange))
+
+        Just (Elm.Syntax.Node.Node exposingExplicitRange (Elm.Syntax.Exposing.Explicit exposeSet)) ->
+            Just
+                (Elm.Syntax.Node.Node
+                    exposingExplicitRange
+                    (Elm.Syntax.Exposing.Explicit (exposeSet |> exposeListToNormal))
+                )
+
+        Nothing ->
+            Nothing
+
+
+importsMerge : Elm.Syntax.Import.Import -> Elm.Syntax.Import.Import -> Elm.Syntax.Import.Import
+importsMerge earlier later =
+    { moduleName = earlier.moduleName
+    , moduleAlias =
+        case earlier.moduleAlias of
+            Just alias ->
+                alias |> Just
+
+            Nothing ->
+                later.moduleAlias
+    , exposingList =
+        exposingCombine earlier.exposingList later.exposingList
+    }
+
+
+exposingCombine :
+    Maybe (Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing)
+    -> Maybe (Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing)
+    -> Maybe (Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing)
+exposingCombine a b =
+    case a of
+        Just (Elm.Syntax.Node.Node exposingAllRange (Elm.Syntax.Exposing.All allRange)) ->
+            Just (Elm.Syntax.Node.Node exposingAllRange (Elm.Syntax.Exposing.All allRange))
+
+        Just (Elm.Syntax.Node.Node earlierExposingExplicitRange (Elm.Syntax.Exposing.Explicit earlierExposeSet)) ->
+            Just
+                (case b of
+                    Just (Elm.Syntax.Node.Node exposingAllRange (Elm.Syntax.Exposing.All allRange)) ->
+                        Elm.Syntax.Node.Node exposingAllRange (Elm.Syntax.Exposing.All allRange)
+
+                    Just (Elm.Syntax.Node.Node laterExposingExplicitRange (Elm.Syntax.Exposing.Explicit laterExposeSet)) ->
+                        Elm.Syntax.Node.Node
+                            (case lineOffsetInRange earlierExposingExplicitRange of
+                                Print.NextLine ->
+                                    earlierExposingExplicitRange
+
+                                Print.SameLine ->
+                                    laterExposingExplicitRange
+                            )
+                            (Elm.Syntax.Exposing.Explicit
+                                (earlierExposeSet ++ laterExposeSet |> exposeListToNormal)
+                            )
+
+                    Nothing ->
+                        Elm.Syntax.Node.Node earlierExposingExplicitRange (Elm.Syntax.Exposing.Explicit earlierExposeSet)
+                )
+
+        Nothing ->
+            b
+
+
 lineOffsetBetweenNodes : Elm.Syntax.Node.Node a -> Elm.Syntax.Node.Node b -> Print.LineOffset
 lineOffsetBetweenNodes (Elm.Syntax.Node.Node earlierRange _) (Elm.Syntax.Node.Node laterRange _) =
     if earlierRange.end.row == laterRange.start.row then
-        Print.NextLine
+        Print.SameLine
 
     else
-        Print.SameLine
+        Print.NextLine
 
 
 lineOffsetInNode : Elm.Syntax.Node.Node a -> Print.LineOffset
@@ -98,23 +213,23 @@ lineOffsetInNode (Elm.Syntax.Node.Node range _) =
 lineOffsetInRange : Elm.Syntax.Range.Range -> Print.LineOffset
 lineOffsetInRange range =
     if range.start.row == range.end.row then
-        Print.NextLine
+        Print.SameLine
 
     else
-        Print.SameLine
+        Print.NextLine
 
 
 lineOffsetBetweenNodeList : Elm.Syntax.Node.Node a -> Elm.Syntax.Node.Node b -> Print.LineOffset
 lineOffsetBetweenNodeList (Elm.Syntax.Node.Node earlierRange _) (Elm.Syntax.Node.Node laterRange _) =
     if earlierRange.end.row == laterRange.start.row then
-        Print.NextLine
-
-    else
         Print.SameLine
 
+    else
+        Print.NextLine
 
-exposing_ : Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing -> Print
-exposing_ (Elm.Syntax.Node.Node exposingRange syntaxExposing) =
+
+exposing_ : LineOffset -> Elm.Syntax.Node.Node Elm.Syntax.Exposing.Exposing -> Print
+exposing_ lineOffset (Elm.Syntax.Node.Node exposingRange syntaxExposing) =
     Print.symbol "("
         |> Print.followedBy
             (case syntaxExposing of
@@ -131,8 +246,10 @@ exposing_ (Elm.Syntax.Node.Node exposingRange syntaxExposing) =
 
                         _ :: _ :: _ ->
                             commaSeparated
-                                (lineOffsetInRange exposingRange)
+                                lineOffset
                                 (exposingSet
+                                    |> List.sortWith
+                                        (\(Elm.Syntax.Node.Node _ a) (Elm.Syntax.Node.Node _ b) -> exposeCompare a b)
                                     |> List.map
                                         (\(Elm.Syntax.Node.Node _ syntaxExpose) -> expose syntaxExpose)
                                 )
@@ -142,7 +259,13 @@ exposing_ (Elm.Syntax.Node.Node exposingRange syntaxExposing) =
 
 commaSeparated : Print.LineOffset -> List Print -> Print
 commaSeparated lineOffset elements =
-    Print.emptiableLayoutPositiveIndent lineOffset
+    (case lineOffset of
+        Print.SameLine ->
+            Print.empty
+
+        Print.NextLine ->
+            Print.space
+    )
         |> Print.followedBy
             (Print.inSequence
                 (elements
@@ -150,7 +273,7 @@ commaSeparated lineOffset elements =
                         (\elementPrint ->
                             elementPrint
                                 |> Print.followedBy
-                                    (Print.layoutPositiveIndent lineOffset)
+                                    (Print.emptiableLayoutPositiveIndent lineOffset)
                         )
                     |> List.intersperse
                         (Print.symbol ","
@@ -163,6 +286,110 @@ commaSeparated lineOffset elements =
 moduleName : Elm.Syntax.ModuleName.ModuleName -> Print
 moduleName syntaxModuleName =
     Print.symbol (syntaxModuleName |> String.join ".")
+
+
+exposeCompare : Elm.Syntax.Exposing.TopLevelExpose -> Elm.Syntax.Exposing.TopLevelExpose -> Basics.Order
+exposeCompare a b =
+    case a of
+        Elm.Syntax.Exposing.InfixExpose aOperatorSymbol ->
+            case b of
+                Elm.Syntax.Exposing.InfixExpose bOperatorSymbol ->
+                    compare aOperatorSymbol bOperatorSymbol
+
+                Elm.Syntax.Exposing.FunctionExpose _ ->
+                    LT
+
+                Elm.Syntax.Exposing.TypeOrAliasExpose _ ->
+                    LT
+
+                Elm.Syntax.Exposing.TypeExpose _ ->
+                    LT
+
+        Elm.Syntax.Exposing.FunctionExpose aName ->
+            case b of
+                Elm.Syntax.Exposing.InfixExpose _ ->
+                    GT
+
+                Elm.Syntax.Exposing.FunctionExpose bName ->
+                    compare aName bName
+
+                Elm.Syntax.Exposing.TypeOrAliasExpose _ ->
+                    GT
+
+                Elm.Syntax.Exposing.TypeExpose _ ->
+                    GT
+
+        Elm.Syntax.Exposing.TypeOrAliasExpose aName ->
+            case b of
+                Elm.Syntax.Exposing.InfixExpose _ ->
+                    GT
+
+                Elm.Syntax.Exposing.FunctionExpose _ ->
+                    LT
+
+                Elm.Syntax.Exposing.TypeOrAliasExpose bName ->
+                    compare aName bName
+
+                Elm.Syntax.Exposing.TypeExpose bTypeExpose ->
+                    compare aName bTypeExpose.name
+
+        Elm.Syntax.Exposing.TypeExpose aTypeExpose ->
+            case b of
+                Elm.Syntax.Exposing.InfixExpose _ ->
+                    GT
+
+                Elm.Syntax.Exposing.FunctionExpose _ ->
+                    LT
+
+                Elm.Syntax.Exposing.TypeOrAliasExpose bName ->
+                    compare aTypeExpose.name bName
+
+                Elm.Syntax.Exposing.TypeExpose bTypeExpose ->
+                    compare aTypeExpose.name bTypeExpose.name
+
+
+exposesCombine : List Elm.Syntax.Exposing.TopLevelExpose -> List Elm.Syntax.Exposing.TopLevelExpose
+exposesCombine syntaxExposes =
+    case syntaxExposes of
+        [] ->
+            []
+
+        [ onlyExpose ] ->
+            [ onlyExpose ]
+
+        expose0 :: expose1 :: expose2Up ->
+            case exposeCompare expose0 expose1 of
+                EQ ->
+                    exposesCombine
+                        (exposeMerge expose0 expose1 :: expose2Up)
+
+                -- LT | GT
+                _ ->
+                    expose0 :: exposesCombine (expose1 :: expose2Up)
+
+
+exposeMerge : Elm.Syntax.Exposing.TopLevelExpose -> Elm.Syntax.Exposing.TopLevelExpose -> Elm.Syntax.Exposing.TopLevelExpose
+exposeMerge a b =
+    case a of
+        Elm.Syntax.Exposing.TypeExpose aTypeExpose ->
+            case b of
+                Elm.Syntax.Exposing.TypeExpose bTypeExpose ->
+                    Elm.Syntax.Exposing.TypeExpose
+                        { name = aTypeExpose.name
+                        , open =
+                            case aTypeExpose.open of
+                                Just openRange ->
+                                    Just openRange
+
+                                Nothing ->
+                                    bTypeExpose.open
+                        }
+
+                _ ->
+                    a
+
+        _ ->
+            b
 
 
 expose : Elm.Syntax.Exposing.TopLevelExpose -> Print
@@ -183,7 +410,5 @@ expose syntaxExpose =
                     Print.symbol syntaxExposeType.name
 
                 Just _ ->
-                    Print.symbol "("
-                        |> Print.followedBy
-                            (Print.symbol syntaxExposeType.name)
-                        |> Print.followedBy (Print.symbol ")")
+                    Print.symbol syntaxExposeType.name
+                        |> Print.followedBy (Print.symbol "(..)")
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -55,5 +55,118 @@ import List
                         |> expectPrintedAs
                             """import List as CoreList"""
                 )
+            , Test.test "name + alias + exposing all, already same line"
+                (\() ->
+                    """module A exposing (..)
+import List as CoreList exposing (..)"""
+                        |> expectPrintedAs
+                            """import List as CoreList exposing (..)"""
+                )
+            , Test.test "name + alias + exposing all, multiline"
+                (\() ->
+                    """module A exposing (..)
+import List
+    as CoreList
+        exposing (..
+            )"""
+                        |> expectPrintedAs
+                            """import List as CoreList exposing (..)"""
+                )
+            , Test.test "name + alias + exposing one, already same line"
+                (\() ->
+                    """module A exposing (..)
+import List as CoreList exposing (map)"""
+                        |> expectPrintedAs
+                            """import List as CoreList exposing (map)"""
+                )
+            , Test.test "name + alias + exposing one, multiline"
+                (\() ->
+                    """module A exposing (..)
+import List as CoreList
+    exposing (map
+    )"""
+                        |> expectPrintedAs
+                            """import List as CoreList exposing (map)"""
+                )
+            , Test.test "name + alias + exposing multiple, already same line"
+                (\() ->
+                    """module A exposing (..)
+import List as CoreList exposing (filter, map)"""
+                        |> expectPrintedAs
+                            """import List as CoreList exposing (filter, map)"""
+                )
+            , Test.test "name + alias + exposing multiple, multiline"
+                (\() ->
+                    """module A exposing (..)
+import List as CoreList exposing (filter,
+    map)"""
+                        |> expectPrintedAs
+                            """import List as CoreList
+    exposing
+        ( filter
+        , map
+        )"""
+                )
+            , Test.test "exposes get sorted"
+                (\() ->
+                    """module A exposing (..)
+import List exposing (map, filter)"""
+                        |> expectPrintedAs
+                            """import List exposing (filter, map)"""
+                )
+            , Test.test "exposes get deduplicated"
+                (\() ->
+                    """module A exposing (..)
+import List exposing (List, filter, map, filter, List)"""
+                        |> expectPrintedAs
+                            """import List exposing (List, filter, map)"""
+                )
+            , Test.test "open type exposes get deduplicated"
+                (\() ->
+                    """module A exposing (..)
+import Maybe exposing (Maybe(..), map, Maybe)"""
+                        |> expectPrintedAs
+                            """import Maybe exposing (Maybe(..), map)"""
+                )
+            , Test.test "open type exposes get deduplicated across imports"
+                (\() ->
+                    """module A exposing (..)
+import Maybe exposing (Maybe(..), map)
+import Maybe exposing (Maybe)"""
+                        |> expectPrintedAs
+                            """import Maybe exposing (Maybe(..), map)"""
+                )
+            , Test.test "exposes get deduplicated across imports, preserving line offset of higher"
+                (\() ->
+                    """module A exposing (..)
+import Maybe exposing (Maybe)
+import Maybe exposing (Maybe(..),
+    map)"""
+                        |> expectPrintedAs
+                            """import Maybe
+    exposing
+        ( Maybe(..)
+        , map
+        )"""
+                )
+            , Test.test "import aliases get deduplicated across imports"
+                (\() ->
+                    """module A exposing (..)
+import List exposing (map)
+import List as CoreList"""
+                        |> expectPrintedAs
+                            """import List as CoreList exposing (map)"""
+                )
+            , Test.test "imports get sorted"
+                (\() ->
+                    """module A exposing (..)
+import A
+import C
+import B"""
+                        |> expectPrintedAs
+                            """import A
+import B
+import C"""
+                )
             ]
         ]
