f643cdd4c003dff698e6f8b606cd25a7eb4a414f
lue-bird <lue.the.bird@gmail.com>
2024-11-20 00:52:44 +0100

    node-elm-syntax-format document v8 snapshot progress

--- node-elm-syntax-format/src/index.js
+++ node-elm-syntax-format/src/index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 import * as Node from "@lue-bird/elm-state-interface-experimental/node"
+// import * as v8 from "node:v8"
 import { Elm } from "./elmMain.js"
 
 const elmApp = Elm.Main.init()
+// ↓ workers not yet implemented, yet
+// ↓ https://github.com/nodejs/node/issues/44014
+// v8.startupSnapshot.setDeserializeMainFunction(() => {
 Node.programStart({ ports: elmApp.ports })
+// })
